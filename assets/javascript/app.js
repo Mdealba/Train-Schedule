b3261865b54ec0e6bcdfb32c94b5e8ae039e7824
Mdealba <M_dealba@yahoo.com>
2018-05-10 10:11:52 -0700

    Wire-Frame & CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQM3dXTgF3bkaKLpyGj2K9bTrS0eNndRs",
+    authDomain: "start-demo.firebaseapp.com",
+    databaseURL: "https://start-demo.firebaseio.com",
+    projectId: "start-demo",
+    storageBucket: "start-demo.appspot.com",
+    messagingSenderId: "405222557243"
+  };
+  firebase.initializeApp(config);
+
+  
